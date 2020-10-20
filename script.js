3463c3bbed0c18e35d04a36321696a78e8053e38
unknown <dcoromina8@boscdelacoma.cat>
2020-10-20 16:15:02 +0200

    botons

---
+++ script.js
@@ -0,0 +1,39 @@
+function do1() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function re() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function mi() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function fa() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function sol() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function la() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function si() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
+
+function do2() {
+    var audio = new Audio("audio/do.wav");
+    audio.play();
+}
